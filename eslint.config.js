58d277381f4682cbbe00430d82567601ce5b928c
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-03-10 00:03:11 +0800

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import { includeIgnoreFile } from '@eslint/compat';
+import stylistic from '@stylistic/eslint-plugin';
 import svelteConfig from './svelte.config.js';
 import { defineConfig } from 'eslint/config';
 import svelte from 'eslint-plugin-svelte';
@@ -8,32 +9,38 @@ import path from 'node:path';
 import js from '@eslint/js';
 
 export default defineConfig(
-	includeIgnoreFile(path.resolve(import.meta.dirname, '.gitignore')),
-	js.configs.recommended,
-	...ts.configs.recommended,
-	...svelte.configs.recommended,
-	{
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node
-			}
-		},
-		rules: {}
-	},
-	{
-		files: [
-			'**/*.svelte',
-			'**/*.svelte.ts',
-			'**/*.svelte.js'
-		],
-		languageOptions: {
-			parserOptions: {
-				projectService: true,
-				extraFileExtensions: ['.svelte'],
-				parser: ts.parser,
-				svelteConfig
-			}
-		}
-	}
+    includeIgnoreFile(path.resolve(import.meta.dirname, '.gitignore')),
+    js.configs.recommended,
+    ...ts.configs.recommended,
+    ...svelte.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node
+            }
+        },
+        plugins: {
+            '@stylistic': stylistic
+        },
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@stylistic/indent': 'error'
+        }
+    },
+    {
+        files: [
+            '**/*.svelte',
+            '**/*.svelte.ts',
+            '**/*.svelte.js'
+        ],
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                extraFileExtensions: ['.svelte'],
+                parser: ts.parser,
+                svelteConfig
+            }
+        }
+    }
 );
