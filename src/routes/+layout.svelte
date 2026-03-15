a812dff1b3129b4e929e0e4bc5a6c76fe291cf24
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-03-15 13:27:16 +0800

    feat(styles): add variable fonts and update layout styles for improved typography

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-	import '$lib/styles/layout.css';
-	import { ModeWatcher } from 'mode-watcher';
-	import { Toaster } from '../lib/components/ui/sonner';
 	import { deepMerge, MetaTags } from 'svelte-meta-tags';
+	import { Toaster } from '../lib/components/ui/sonner';
+	import '@fontsource-variable/google-sans-flex';
+	import '@fontsource-variable/google-sans-code';
+	import '@fontsource-variable/playfair-display';
+	import { ModeWatcher } from 'mode-watcher';
 	import { page } from '$app/state';
+	import '$lib/styles/layout.css';
 
 	let { children, data } = $props();
 </script>
