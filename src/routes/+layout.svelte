04ccdc794cb30c92add35d3d5e81bfbc5b99f6db
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-03-19 02:07:45 +0800

    feat(layout): implement theme toggle and selection in layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,11 @@
 	import '@fontsource-variable/google-sans-flex';
 	import '@fontsource-variable/google-sans-code';
 	import '@fontsource-variable/playfair-display';
-	import { ModeWatcher } from 'mode-watcher';
+	import { mode, ModeWatcher, setTheme, theme, toggleMode } from 'mode-watcher';
 	import { page } from '$app/state';
-	import '$lib/styles/layout.css';
+	import { Button } from '../lib/components/ui/button/index.js';
+	import { MoonIcon, SunIcon } from '@lucide/svelte';
+	import { Select, SelectContent, SelectItem, SelectTrigger } from '$lib/components/ui/select';
 
 	let { children, data } = $props();
 </script>
@@ -14,5 +16,24 @@
 <Toaster/>
 <ModeWatcher/>
 <MetaTags {...deepMerge(data.baseMetaTags, page.data.pageMetaTags)}/>
+
+<div class="flex">
+    <Button onclick={() => toggleMode()}>
+        {#if mode.current == 'light'}
+            <MoonIcon/>
+        {:else}
+            <SunIcon/>
+        {/if}
+    </Button>
+    <Select type="single" onValueChange={(value) => setTheme(value)}>
+        <SelectTrigger>
+            {theme.current || 'Select Theme'}
+        </SelectTrigger>
+        <SelectContent>
+            <SelectItem value="default">Default</SelectItem>
+            <SelectItem value="rose">Rose</SelectItem>
+        </SelectContent>
+    </Select>
+</div>
 
 {@render children()}
