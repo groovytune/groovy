64afef31d6d11ae8899f29a9f7a6d987c3d86a79
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-03-19 19:40:47 +0800

    refactor(layout): remove theme toggle and selection components for cleaner layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,9 @@
 	import '@fontsource-variable/google-sans-flex';
 	import '@fontsource-variable/google-sans-code';
 	import '@fontsource-variable/playfair-display';
-	import { mode, ModeWatcher, setTheme, theme, toggleMode } from 'mode-watcher';
+	import { ModeWatcher } from 'mode-watcher';
 	import { page } from '$app/state';
-	import { Button } from '../lib/components/ui/button/index.js';
-	import { MoonIcon, SunIcon } from '@lucide/svelte';
-	import { Select, SelectContent, SelectItem, SelectTrigger } from '$lib/components/ui/select';
+	import '$lib/styles/app.css';
 
 	let { children, data } = $props();
 </script>
@@ -16,24 +14,5 @@
 <Toaster/>
 <ModeWatcher/>
 <MetaTags {...deepMerge(data.baseMetaTags, page.data.pageMetaTags)}/>
-
-<div class="flex">
-    <Button onclick={() => toggleMode()}>
-        {#if mode.current == 'light'}
-            <MoonIcon/>
-        {:else}
-            <SunIcon/>
-        {/if}
-    </Button>
-    <Select type="single" onValueChange={(value) => setTheme(value)}>
-        <SelectTrigger>
-            {theme.current || 'Select Theme'}
-        </SelectTrigger>
-        <SelectContent>
-            <SelectItem value="default">Default</SelectItem>
-            <SelectItem value="rose">Rose</SelectItem>
-        </SelectContent>
-    </Select>
-</div>
 
 {@render children()}
