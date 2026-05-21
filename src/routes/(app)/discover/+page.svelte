8b660e7b9b8a6885ab376d435f27fcd408322fa2
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-05-21 21:55:38 +0800

    feat: enhance chart display with horizontal scroll and improved layout

--- src/routes/(app)/discover/+page.svelte
+++ src/routes/(app)/discover/+page.svelte
@@ -120,43 +120,47 @@
     {/each}
 {/snippet}
 
-<section class="px-5">
-    <h1 class="text-2xl sm:text-4xl font-bold my-4 flex items-center gap-2">
+<section>
+    <h1 class="text-2xl sm:text-4xl font-bold my-4 flex items-center gap-2 px-5">
         <StarIcon class="text-primary size-7 sm:size-8"/>
         Charts
     </h1>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {@render Chart(
-            'Top 100 Tracks',
-            'Explore the most popular tracks on Groovy. Discover new music trends and find your next favorite song.',
-            resolve('/(app)/discover/chart'),
-            'radial-gradient( circle farthest-corner at 10% 20%,  rgba(170,245,248,1) 0%, rgba(248,162,239,0.8) 90% )',
-            'black',
-            'lg:col-span-2 xl:col-span-1'
-        )}
-        {@render Chart(
-            'Top Pop Tracks',
-            'Whether you\'re a fan of catchy melodies or powerful vocals, our chart has something for every pop enthusiast.',
-            resolve('/(app)/discover/chart') + '?genre=pop',
-            'linear-gradient( 101.9deg,  rgba(239,151,189,1) -13.9%, rgba(204,239,243,1) 34.6%, rgba(227,241,202,1) 54.2%, rgba(237,187,187,1) 88.6% )',
-            'black'
-        )}
-        {@render Chart(
-            'Top Hip Hop Tracks',
-            'Explore the hottest hip hop tracks on Groovy. Discover emerging artists and classic hits that define the genre.',
-            resolve('/(app)/discover/chart') + '?genre=hiphop',
-            'radial-gradient( circle farthest-corner at -8.8% -6.6%,  rgba(255,206,78,1) 0%, rgba(253,169,124,1) 70.1% )',
-            'black'
-        )}
-        {@render Chart(
-            'Top Rock Tracks',
-            'Discover the best rock tracks on Groovy. From classic rock anthems to modern hits, our chart has something for every rock fan.',
-            resolve('/(app)/discover/chart') + '?genre=rock',
-            'linear-gradient( 91.7deg,  rgba(135,206,235,1) 7.3%, rgba(255,154,139,1) 40.3%, rgba(255,195,160,1) 57.9%, rgba(255,215,0,1) 93.5% )',
-            'black',
-            'lg:col-span-2 xl:col-span-1'
-        )}
-    </div>
+    <ScrollArea orientation="horizontal">
+        <div class="md:grid flex md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 overflow-auto px-5">
+            {@render Chart(
+                'Top 100 Tracks',
+                'Explore the most popular tracks on Groovy. Discover new music trends and find your next favorite song.',
+                resolve('/(app)/discover/chart'),
+                'radial-gradient( circle farthest-corner at 10% 20%,  rgba(170,245,248,1) 0%, rgba(248,162,239,0.8) 90% )',
+                'black',
+                'lg:col-span-2 xl:col-span-1 shrink-0 w-96 md:w-full'
+            )}
+            {@render Chart(
+                'Top Pop Tracks',
+                'Whether you\'re a fan of catchy melodies or powerful vocals, our chart has something for every pop enthusiast.',
+                resolve('/(app)/discover/chart') + '?genre=pop',
+                'linear-gradient( 101.9deg,  rgba(239,151,189,1) -13.9%, rgba(204,239,243,1) 34.6%, rgba(227,241,202,1) 54.2%, rgba(237,187,187,1) 88.6% )',
+                'black',
+                'md:w-full shrink-0 w-96 md:w-full'
+            )}
+            {@render Chart(
+                'Top Hip Hop Tracks',
+                'Explore the hottest hip hop tracks on Groovy. Discover emerging artists and classic hits that define the genre.',
+                resolve('/(app)/discover/chart') + '?genre=hiphop',
+                'radial-gradient( circle farthest-corner at -8.8% -6.6%,  rgba(255,206,78,1) 0%, rgba(253,169,124,1) 70.1% )',
+                'black',
+                'shrink-0 w-96 md:w-full'
+            )}
+            {@render Chart(
+                'Top Rock Tracks',
+                'Discover the best rock tracks on Groovy. From classic rock anthems to modern hits, our chart has something for every rock fan.',
+                resolve('/(app)/discover/chart') + '?genre=rock',
+                'linear-gradient( 91.7deg,  rgba(135,206,235,1) 7.3%, rgba(255,154,139,1) 40.3%, rgba(255,195,160,1) 57.9%, rgba(255,215,0,1) 93.5% )',
+                'black',
+                'lg:col-span-2 xl:col-span-1 shrink-0 w-96 md:w-full'
+            )}
+        </div>
+    </ScrollArea>
 </section>
 <section class="mt-10">
     <h1 class="text-2xl sm:text-4xl font-bold my-4 flex items-center gap-2 px-5">
