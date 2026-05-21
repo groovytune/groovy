d549363d3f1fcb8ed45d063fe995c4bf88994882
Cat++ <69035887+catplvsplus@users.noreply.github.com>
2026-05-21 23:47:37 +0800

    feat: implement follow, unfollow, and check following status for artists

---
+++ src/routes/(app)/api/artist/[artistId]/follow/+server.ts
@@ -0,0 +1,74 @@
+import { error, json } from '@sveltejs/kit';
+import { prisma } from '$lib/server/prisma';
+
+export async function GET({ params, locals }) {
+    if (!locals.user) {
+        return error(401, 'Unauthorized');
+    }
+
+    const data = await prisma.userFollow.findUnique({
+        where: {
+            followerId_userId: {
+                userId: params.artistId,
+                followerId: locals.user.id
+            }
+        }
+    });
+
+    return json({
+        following: !!data,
+        data: data
+    });
+}
+
+export async function POST({ params, locals }) {
+    if (!locals.user) {
+        return json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    let data = await prisma.userFollow.findUnique({
+        where: {
+            followerId_userId: {
+                userId: params.artistId,
+                followerId: locals.user.id
+            }
+        }
+    });
+
+    data ??= await prisma.userFollow.create({
+        data: {
+            userId: params.artistId,
+            followerId: locals.user.id
+        }
+    });
+
+    return json(data);
+}
+
+export async function DELETE({ params, locals }) {
+    if (!locals.user) {
+        return json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    const hasLiked = await prisma.userFollow.findUnique({
+        where: {
+            followerId_userId: {
+                userId: params.artistId,
+                followerId: locals.user.id
+            }
+        }
+    });
+
+    if (hasLiked) {
+        await prisma.userFollow.delete({
+            where: {
+                followerId_userId: {
+                    userId: params.artistId,
+                    followerId: locals.user.id
+                }
+            }
+        });
+    }
+
+    return json({ success: true });
+}
